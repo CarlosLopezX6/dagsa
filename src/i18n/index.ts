5975e00cedeedf8917fb725df09880ffc9544f48
Carlos Lopez <ingcarlosfranciscolj@outlook.com>
2025-04-30 03:21:44 -0600

    Añadiendo internacionalizacion

---
+++ src/i18n/index.ts
@@ -0,0 +1,17 @@
+import english from "./en.json";
+import spanish from "./es.json";
+
+
+const LANGUAGES = {
+    ENGLISH: "en",
+    SPANISH: "es"
+}
+
+
+export const getI18N = ({ currentLocale = "en" }: { currentLocale: string | undefined } ) => {
+
+    if (currentLocale === LANGUAGES.SPANISH) return { ...english, ...spanish };
+
+    return english
+
+}
---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const defaultLang = 'en';
+
+export const languages: Record<
+	string,
+	{ code: string; name: string; }
+> = {
+	en: {
+		code: 'en',
+		name: 'English'
+	},
+	es: {
+		code: 'es',
+		name: 'Español'
+	},
+};
